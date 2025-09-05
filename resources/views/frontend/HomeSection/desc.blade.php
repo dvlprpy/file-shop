edc4097bfe1545d9867302cf5c7190dd33d588c5
dvlprpy <jsbxbxbwj@outlook.com>
2025-09-05 16:25:44 +0330

    Add write interval effect

--- resources/views/frontend/HomeSection/desc.blade.php
+++ resources/views/frontend/HomeSection/desc.blade.php
@@ -1,4 +1,19 @@
-<p class="p-desc text-center">
-    به بهترین فایل‌ها و پکیج‌های آموزشی دسترسی داشته باشید. کیفیت و تجربه کاربری بالا تضمین شده است.
-</p>
+<p class="p-desc text-center"></p>
 <hr>
+
+<script>
+    let pDesc = document.querySelector('p.p-desc');
+    let text = "به بهترین فایل‌ها و پکیج‌های آموزشی دسترسی داشته باشید. کیفیت و تجربه کاربری بالا تضمین شده است.";
+    let index = 0;
+
+    let interval = setInterval(() => {
+
+        pDesc.textContent += text[index];
+        index++;
+
+
+        if (index >= text.length) {
+            clearInterval(interval);
+        }
+    }, 100); 
+</script>
